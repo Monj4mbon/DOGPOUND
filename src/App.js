b8f36c1cf504dbcc810b58c3f905b49dd52dc81a
ilyasse el haddad <68947535+ilyasse406@users.noreply.github.com>
2020-10-20 14:40:08 +0200

    debut projet 2 !

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Liste from "./components/Liste";
 
 export default function App(){
   return (
     <div className="container">
       <div className="text-center">
-        
-
+        <h1>fiche de présence</h1>
+        <input type="text" className="w-50"/>
+        <button className="w-25">Ajouter</button>
       </div>
     </div>
   );
