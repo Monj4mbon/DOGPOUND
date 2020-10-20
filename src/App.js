82c4717eb6d2941050a0c76c49dfc6133b0ecb32
ilyasse el haddad <68947535+ilyasse406@users.noreply.github.com>
2020-10-20 16:11:39 +0200

    ajout fonction ajouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import Liste from "./components/Liste";
 
-export default function App(){
-  return (
-    <div className="container">
-      <div className="text-center">
-        <h1>fiche de présence</h1>
-        <input type="text" className="w-50"/>
-        <button className="w-25">Ajouter</button>
-      </div>
-    </div>
-  );
-};
+export default class App extends React.Component{
+constructor() {
+  super();
+  this.state = {
+    input : "",
+    value : [],
+  }
+}
+ajouter = () => {
+let x = this.state;
+if (x.input == "") {
+  return
+  
+} else {
+  let objet = {
+    valeur : x.input,
+    validate : false
+  }
+  x.value.push(objet);
+  x.input = "";
+}
+
+this.setState(x)
+}
+
+
+handleChange(e) {
+  this.setState({ input: e.target.value })
+}
+render(){
+return(
+  <div className="container">
+  <div className="text-center">
+    <h1>fiche de présence</h1>
+    <input type="text" className="w-50" onChange={e => this.handleChange(e)} value={this.state.input}/>
+    <button className="w-25" onClick={() => this.ajouter()}>Ajouter</button>
+  </div>
+  <ul>
+    {
+      this.state.value.map((e,i) => {
+        return(
+          < Liste key={i}  valeur={e.valeur}/>
+        )
+        
+      })
+    } 
+  </ul>
+</div>
+)
+}
+}
--- src/components/Liste.js
+++ src/components/Liste.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-export default class Liste extends React.Component{
-render(){
-return(
-""
-)
-}
+export default class Liste extends React.Component {
+    render() {
+        return (
+            <li>
+                <span>{this.props.valeur}</span>
+                <button>Présent(e)</button>
+                <button>Retard</button>
+                <button>Absent(e)</button>
+            </li>
+        )
+    }
 }
